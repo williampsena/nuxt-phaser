ba48a339f4784d27a1e525d9c239c2cd1b4672c5
William Sena <will@panthro.localdomain>
2023-05-04 20:04:48 -0300

    Add exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { defineNuxtPlugin } from '#app'
 import VuePhaserPlugin from './vue.index'
-export * as PhaserGame from './phaserGame.vue'
+import PhaserGame from './phaserGame.vue'
+
+export { PhaserGame, VuePhaserPlugin }
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(VuePhaserPlugin)
