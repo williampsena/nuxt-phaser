36fe596ef25d7257e35feb3e02b464254ec80da6
William Sena <will@panthro.localdomain>
2023-05-07 22:55:43 -0300

    Remove index loader

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import PhaserGame from './phaserGame.vue'
-import VuePhaserPlugin from './vue.index'
-import NuxtPhaserPlugin from './plugin'
-
-export { PhaserGame, VuePhaserPlugin, NuxtPhaserPlugin }
-
-export default NuxtPhaserPlugin
