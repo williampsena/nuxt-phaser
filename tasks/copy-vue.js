c882c7a90aa6fd83a202de9ad5923e52f80da5b8
William Sena <will@panthro.localdomain>
2023-05-07 22:08:46 -0300

    Include vue file

--- tasks/copy-vue.js
+++ tasks/copy-vue.js
@@ -1,4 +1,6 @@
 const shelljs = require('shelljs')
 
-shelljs.cp('./src/phaserGame.vue', './dist')
+shelljs.cp('./src/phaserGame.vue', './dist/esm')
+shelljs.cp('./src/phaserGame.vue', './dist/umd')
+shelljs.cp('./src/phaserGame.vue', './dist/cjs')
 shelljs.rm('./dist/*/*.spec.js')
