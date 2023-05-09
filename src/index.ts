2620874cf5a5b7461f0fcbaeeb271b9c7e713d0e
William Sena <me@willsena.dev>
2023-05-09 13:10:50 -0300

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import PhaserGame from './phaserGame.vue'
 import VuePhaserPlugin from './vue.index'
 import NuxtPhaserPlugin from './plugin'
-
+export * from '../types/global'
 export { PhaserGame, VuePhaserPlugin, NuxtPhaserPlugin }
 
 export default NuxtPhaserPlugin
